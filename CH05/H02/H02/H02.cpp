3d1a64635fe8f1eafdaba49ead49efc2000c660c
VIGILIKI-X1\zajac(GitHub_Desktop) <zajac.mc@gmail.com>
2018-03-09 17:21:53 +0100

    Rozdzial 05 zadanie 05

--- CH05/H02/H02/H02.cpp
+++ CH05/H02/H02/H02.cpp
@@ -1,16 +1,23 @@
 // Marcin Zajac 09 03 2018
-// Stroustrup Rozdzial 05 H03
+// Stroustrup Rozdzial 05 H05
 
 #include "../../../std_lib_facilities.h"
 
 const double c_min = -273.15;
 const double roznica_KC = 273.15;
+const double k_min = 0;
 
 double ctok(double c) // Konwertuje stopnie w skali Celcjusza na skale Kelwina
 {
-	if (c < c_min) error("Podano temperature ponizej zera absolutnego", c);
+	if (c < c_min) error("Podano temperature ponizej zera absolutnego(C)", c);
 	double k = c + roznica_KC;
 	return k;
+}
+
+double ktoc(double k)   //Konwertuje z K na C
+{
+	if (k<k_min) error("Podales temperature ponizej zera absolutnego(K)", k);
+	return k - roznica_KC;
 }
 
 int main()
@@ -19,8 +26,12 @@ try
 	double c = 0; // Zmienna do odbierania danych wejsciowych
 	cout << "Podaj temperature w C, ktora chcesz przekonwertowac na K: ";
 	cin >> c;	// Pobiernanie temperatury do zmiennej wejsciowej
-	double k = ctok(c); // konwersja temperatury
-	cout << k << endl;	// drukowanie temperatury
+	cout << ctok(c) << endl;	// konwersja i drukowanie temperatury
+
+	cout << "Podaj temperature w K, ktora chesz przekonwertowac na C: ";
+	double k = 0;
+	cin >> k;
+	cout << ktoc(k) << endl;
 	keep_window_open();
 }
 catch (exception& e) {
