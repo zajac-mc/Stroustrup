acc1d33753eed67ffd00319b7b6502660be68a7f
VIGILIKI-X1\zajac(GitHub_Desktop) <zajac.mc@gmail.com>
2018-03-12 15:59:03 +0100

    Rozdzial 05 zadanie 10

--- CH05/H08/H08/H08.cpp
+++ CH05/H08/H08/H08.cpp
@@ -1,5 +1,5 @@
 // Marcin Zajac 12 03 2018
-// Stroustrup Rozdzial 05 H08
+// Stroustrup Rozdzial 05 H10
 
 /*
 Autor polecenia w podanym przez siebie rozwiazaniu najpierw pobiera ilosc elementow do zsumowania, a dopiero potem prosi o wprowadzenie serii.
@@ -17,8 +17,8 @@ try
 	if (ile_dodac < 1) error("Liczba elementow do zsumowania musi byc liczba dodatnia!\n");
 
 	cout << "Podaj elementy serii, aby zakonczyc wpisz'|':\n";
-	vector<int> liczby;
-	int liczba;
+	vector<double> liczby;
+	double liczba;
 	while (cin >> liczba)
 		liczby.push_back(liczba);
 
@@ -28,6 +28,16 @@ try
 	for (int i = 0; i < ile_dodac; ++i)
 		suma += liczby[i];
 	cout << "Suma pierwszych " << ile_dodac << " liczb wynosi: " << suma << endl;
+
+	if (ile_dodac < 2) error("Za malo elementow, aby obliczac roznice miedzy nimi: ",ile_dodac);
+	cout << "Wektor roznic " << ile_dodac - 1 << " pierwszych sasiadujacych ze soba elementow:\n";
+	vector<double> roznice;
+	for (int i = 0; i < ile_dodac-1; ++i) {
+		roznice.push_back(liczby[i] - liczby[i+1]);
+		cout << roznice[i] << ' ';
+	}
+	cout << endl;
+
 	keep_window_open();
 	keep_window_open();
 }
