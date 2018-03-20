ef5805a6aebf32501fdca65f58baf15b8eb29393
VIGILIKI-X1\zajac(GitHub_Desktop) <zajac.mc@gmail.com>
2018-03-21 00:28:12 +0100

    Rozdzial 06 cwiczenie 01

--- CH06/calculator02buggy/calculator02buggy/calculator02buggy.cpp
+++ CH06/calculator02buggy/calculator02buggy/calculator02buggy.cpp
@@ -21,7 +21,7 @@ Happy hunting!
 
 //------------------------------------------------------------------------------
 
-lass Token{
+class Token{
 public:
 	char kind;        // what kind of token
 	double value;     // for numbers: a value 
@@ -63,7 +63,7 @@ void Token_stream::putback(Token t)
 
 //------------------------------------------------------------------------------
 
-Token get()
+Token Token_stream::get()
 {
 	if (full) {       // do we already have a Token ready?
 					  // remove token from buffer
@@ -81,7 +81,7 @@ Token get()
 		return Token(ch);        // let each character represent itself
 	case '.':
 	case '0': case '1': case '2': case '3': case '4':
-	case '5': case '6': case '7': case '9':
+	case '5': case '6': case '7':case'8': case '9':
 	{
 		cin.putback(ch);         // put digit back into the input stream
 		double val;
@@ -112,7 +112,7 @@ double primary()
 	{
 		double d = expression();
 		t = ts.get();
-		if (t.kind != ')') error("')' expected);
+		if (t.kind != ')') error("')' expected");
 			return d;
 	}
 	case '8':            // we use '8' to represent a number
@@ -135,6 +135,7 @@ double term()
 		case '*':
 			left *= primary();
 			t = ts.get();
+			break;
 		case '/':
 		{
 			double d = primary();
@@ -155,7 +156,7 @@ double term()
 // deal with + and -
 double expression()
 {
-	double left = term(;      // read and evaluate a Term
+	double left = term();      // read and evaluate a Term
 	Token t = ts.get();        // get the next token from token stream
 
 	while (true) {
@@ -165,7 +166,7 @@ double expression()
 			t = ts.get();
 			break;
 		case '-':
-			left += term();    // evaluate Term and subtract
+			left -= term();    // evaluate Term and subtract
 			t = ts.get();
 			break;
 		default:
@@ -180,6 +181,7 @@ double expression()
 int main()
 try
 {
+	double val = 0;
 	while (cin) {
 		Token t = ts.get();
 
