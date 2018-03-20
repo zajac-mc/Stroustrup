2a43f3becda4e4179aeeed50f50612504287f155
VIGILIKI-X1\zajac(GitHub_Desktop) <zajac.mc@gmail.com>
2018-03-21 00:02:50 +0100

    Rozdzial 06 - calculator00 - version after chapter 6.7

--- CH06/calculator00v01/calculator00v01/calculator00v01.cpp
+++ CH06/calculator00v01/calculator00v01/calculator00v01.cpp
@@ -22,37 +22,6 @@ public:
 		:kind(ch), value(val) { }
 };
 
-//------------------------------------------------------------------------------
-//		||		// Tego ponizej niebawem nie bedzie
-//		||		// Token get_token() --> ts.get()
-//    \ || /
-//     \||/
-//		\/
-//------------------------------------------------------------------------------
-
-Token get_token()    // read a token from cin
-{
-	char ch;
-	cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
-
-	switch (ch) {
-		//not yet   case ';':    // for "print"
-		//not yet   case 'q':    // for "quit"
-	case '(': case ')': case '+': case '-': case '*': case '/':
-		return Token(ch);        // let each character represent itself
-	case '.':
-	case '0': case '1': case '2': case '3': case '4':
-	case '5': case '6': case '7': case '8': case '9':
-	{
-		cin.putback(ch);         // put digit back into the input stream
-		double val;
-		cin >> val;              // read a floating-point number
-		return Token('8', val);   // let '8' represent "a number"
-	}
-	default:
-		error("Bad token");
-	}
-}
 
 //------------------------------------------------------------------------------
 
@@ -153,8 +122,6 @@ try {
 			ts.putback(t);
 		val = expression();
 	}
-	//while (cin)
-	//	cout << "= " << expression() << '\n';
 	keep_window_open("~0");
 }
 catch (exception& e) {
@@ -174,7 +141,6 @@ double expression()
 {
 	double left = term();      // read and evaluate a Term
 	Token t = ts.get();	// Pobiera nastepny token ze strumienia tokenow.
-	// Token t = get_token();     // get the next token
 	while (true) {
 		switch (t.kind) {
 		case '+':
