7241af1007785a5d05a4d0f46024088e6fd34dbc
VIGILIKI-X1\zajac(GitHub_Desktop) <zajac.mc@gmail.com>
2018-03-28 18:14:32 +0200

    Rozdzial 06 zadanie 06

--- CH06/H05/H05/H05.cpp
+++ CH06/H05/H05/H05.cpp
@@ -1,5 +1,5 @@
 // Marcin Zajac 28 03 2018
-// Stroustrup Rozdzial 06 H05
+// Stroustrup Rozdzial 06 H06
 
 /*
 Sentence:
@@ -22,3 +22,50 @@ Article:
 	"the"
 */
 
+#include "../../../std_lib_facilities.h"
+
+const char end_of_sentence = '.';
+
+bool article(string s) {
+	if (s == "the") return true;
+	else false;
+}
+
+bool noun(string s) {
+	if (s == "birds" || s == "fish" || s == "C++") return true;
+	//else if 
+	else return false;
+}
+
+bool verb(string s) {
+	if (s == "rules" || s == "fly" || s == "swim") return true;
+	else return false;
+}
+
+bool conjunction(string s) {
+	if (s == "and" || s == "or" || s == "but")
+		return true;
+	else return false;
+}
+
+bool is_sentence(string s) {
+
+}
+
+int main()
+try
+{
+
+	return 0;
+}
+catch (exception& e)
+{
+	cerr << "error: " << e.what() << endl;
+	keep_window_open();
+	return 1;
+}
+catch (...) {
+	cerr << "Nieznany blad!" << endl;
+	keep_window_open();
+	return 2;
+}
