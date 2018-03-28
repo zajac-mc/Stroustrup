1b26de4d99ae87796bc25737ebe3d35f13a17dfd
VIGILIKI-X1\zajac(GitHub_Desktop) <zajac.mc@gmail.com>
2018-03-28 17:47:51 +0200

    Rozdzial 06 zadanie 05

---
+++ CH06/H05/H05/H05.cpp
@@ -0,0 +1,24 @@
+// Marcin Zajac 28 03 2018
+// Stroustrup Rozdzial 06 H05
+
+/*
+Sentence:
+	Noun Verb
+	Sentence Conjunction Sentence
+Conjunction:
+	"and"
+	"or"
+	"but"
+Noun:
+	"birds"
+	"fish"
+	"C++"
+	Article Noun
+Verb:
+	"rules"
+	"fly"
+	"swim"
+Article:
+	"the"
+*/
+
