66ab17e2e034ff34a078a66f3a6b6549003a17d9
VIGILIKI-X1\zajac(GitHub_Desktop) <zajac.mc@gmail.com>
2019-04-10 17:42:43 +0200

    Rozdzial 06 zadanie 07 - Gramatyka wyrazen logicznych

---
+++ CH06/H07/H07/H07.cpp
@@ -0,0 +1,21 @@
+// Marcin Zajac 10 04 2018
+// Stroustrup Rozdzial 06 H07
+
+/*
+Napisz gramatyke dla wyrazen logicznych
+!x;		~x;		x|y^z = x|(y^z);	x^y&z = x^(y&z)
+
+F1:
+	F2
+	F1 | F2
+F2:
+	F3
+	F2 ^ F3
+F3:
+	F4
+	F3 & F4
+F4:
+	x
+	!x
+	~x
+*/
